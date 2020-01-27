ed2730660ca5707a8646db44fc7223260e4baba4
Ijas <17mcs22@meaec.edu.in>
2020-01-28 00:48:39 +0530

    Design modified

--- blogs/models.py
+++ blogs/models.py
@@ -11,8 +11,9 @@ from ckeditor_uploader.fields import RichTextUploadingField
 
 class Blog(models.Model):
     title = models.CharField(max_length=100)
-    thumbnail = models.ImageField(upload_to='thumbnails/', blank=True)
+    thumbnail = models.ImageField(upload_to='thumbnails/')
     slug = models.SlugField()
+    is_home = models.BooleanField(default=False)
     content = RichTextUploadingField()
     author = models.CharField(max_length=50)
     date = models.DateTimeField(auto_now=True)
